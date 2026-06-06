f869c5611f964360fdcfb8055075f6380915074d
Debjit Das <debjitdas.intern@gmail.com>
2026-06-07 02:11:24 +0530

    feat(hdlbits): add gate solutions (notgate through bcdadd100)

---
+++ vlsi-rtl-projects/01_HDLBits/gates/wire_decl.v
@@ -0,0 +1,8 @@
+// HDLBits: wire_decl
+module top_module (input a, b, c, d, output out, out_n);
+    wire w1, w2;
+    assign w1    = a & b;
+    assign w2    = c & d;
+    assign out   = w1 | w2;
+    assign out_n = ~out;
+endmodule
