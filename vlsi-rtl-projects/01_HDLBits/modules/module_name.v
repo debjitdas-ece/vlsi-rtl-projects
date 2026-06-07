33cdd815f3f30b1f0830df4082a48ae28d145158
Debjit Das <debjitdas.intern@gmail.com>
2026-06-07 22:18:04 +0530

    feat(hdlbits): add module solutions (module through module_addsub)

---
+++ vlsi-rtl-projects/01_HDLBits/modules/module_name.v
@@ -0,0 +1,7 @@
+// HDLBits: module_name
+module top_module (input a, b, c, d, output out1, out2);
+    mod_a inst (
+        .in1(a), .in2(b), .in3(c), .in4(d),
+        .out1(out1), .out2(out2)
+    );
+endmodule
