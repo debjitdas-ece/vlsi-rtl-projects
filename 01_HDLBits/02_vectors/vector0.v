3570ded63e6e67a485c9e52b95262a6329b6cf0f
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:14:22 +0530

    feat(hdlbits): add vector and basic gate solutions (p4-19)

---
+++ 01_HDLBits/02_vectors/vector0.v
@@ -0,0 +1,7 @@
+// HDLBits Problem 11: 3-bit vector
+// Author: Debjit Das | JGEC ECE
+
+module top_module (input [2:0] vec, output [2:0] outv, output o2, o1, o0);
+    assign outv = vec;
+    assign {o2, o1, o0} = vec;
+endmodule
