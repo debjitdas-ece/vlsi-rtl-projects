d26f4e55f435a12f30880b1594094b010e0743f0
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:14:19 +0530

    feat(hdlbits): add vector and basic gate solutions (p4-19)

---
+++ 01_HDLBits/02_vectors/7458.v
@@ -0,0 +1,9 @@
+// HDLBits Problem 10: 7458 chip
+// Author: Debjit Das | JGEC ECE
+
+module top_module (
+    input p1a, p1b, p1c, p1d, p1e, p1f, output p1y,
+    input p2a, p2b, p2c, p2d, output p2y);
+    assign p1y = (p1a & p1b & p1c) | (p1d & p1e & p1f);
+    assign p2y = (p2a & p2b) | (p2c & p2d);
+endmodule
