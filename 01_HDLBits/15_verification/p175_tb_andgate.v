dae6257f968b31e75edca34e16881da9d88308db
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:21:58 +0530

    feat(hdlbits): add verification and testbench solutions (p158-177)

---
+++ 01_HDLBits/15_verification/p175_tb_andgate.v
@@ -0,0 +1,10 @@
+// HDLBits Problem 175: Testbench: andgate
+// Author: Debjit Das | JGEC ECE
+
+module top_module;
+    reg [1:0] in; wire out;
+    andgate dut(.in(in),.out(out));
+    initial begin
+        in=2'b00; #10; in=2'b01; #10; in=2'b10; #10; in=2'b11;
+    end
+endmodule
