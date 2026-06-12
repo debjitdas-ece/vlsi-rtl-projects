728b802ce7a3d3147cb71212193820a7bd95fa1f
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:21:55 +0530

    feat(hdlbits): add verification and testbench solutions (p158-177)

---
+++ 01_HDLBits/15_verification/p174_tb_ab.v
@@ -0,0 +1,8 @@
+// HDLBits Problem 174: Testbench A B
+// Author: Debjit Das | JGEC ECE
+
+module top_module (output reg A, output reg B);
+    initial begin
+        A=0; B=0; #10; A=1; #5; B=1; #5; A=0; #20; B=0;
+    end
+endmodule
