553efcb6a35ce0167cb0510094a36c55ac49d2dc
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:15:09 +0530

    feat(hdlbits): add module hierarchy solutions (p20-28) - adders, DFFs

---
+++ 01_HDLBits/03_modules/module_addsub.v
@@ -0,0 +1,10 @@
+// HDLBits Problem 28: Adder-subtractor
+// Author: Debjit Das | JGEC ECE
+
+module top_module (input [31:0] a, b, input sub, output [31:0] sum);
+    wire cout;
+    wire [31:0] b_xor;
+    assign b_xor = b ^ {32{sub}};
+    add16 lo (.a(a[15:0]),  .b(b_xor[15:0]),  .cin(sub),  .sum(sum[15:0]),  .cout(cout));
+    add16 hi (.a(a[31:16]), .b(b_xor[31:16]), .cin(cout), .sum(sum[31:16]), .cout());
+endmodule
