ac72134426e7e8b9ce8ec168c54baad23a011562
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:14:31 +0530

    feat(hdlbits): add vector and basic gate solutions (p4-19)

---
+++ 01_HDLBits/02_vectors/vectorgates.v
@@ -0,0 +1,9 @@
+// HDLBits Problem 14: Bitwise operators
+// Author: Debjit Das | JGEC ECE
+
+module top_module (input [2:0] a, b,
+    output [2:0] out_or_bitwise, output out_or_logical, output [5:0] out_not);
+    assign out_or_bitwise = a | b;
+    assign out_or_logical = |a | |b;
+    assign out_not        = {~b, ~a};
+endmodule
