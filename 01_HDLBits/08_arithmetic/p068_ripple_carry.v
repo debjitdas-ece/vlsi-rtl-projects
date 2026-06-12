d80806f4efe2c181852982f07012156a14ac4923
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:16:55 +0530

    feat(hdlbits): add arithmetic circuit solutions (p66-72) - adders, BCD

---
+++ 01_HDLBits/08_arithmetic/p068_ripple_carry.v
@@ -0,0 +1,12 @@
+// HDLBits Problem 68: 3-bit binary ripple carry adder
+// Author: Debjit Das | JGEC ECE
+
+module top_module (input [2:0] a, b, input cin,
+    output [2:0] cout, output [2:0] sum);
+    full_adder f0(.a(a[0]),.b(b[0]),.cin(cin),    .cout(cout[0]),.sum(sum[0]));
+    full_adder f1(.a(a[1]),.b(b[1]),.cin(cout[0]),.cout(cout[1]),.sum(sum[1]));
+    full_adder f2(.a(a[2]),.b(b[2]),.cin(cout[1]),.cout(cout[2]),.sum(sum[2]));
+endmodule
+module full_adder (input a, b, cin, output cout, sum);
+    assign {cout, sum} = a + b + cin;
+endmodule
