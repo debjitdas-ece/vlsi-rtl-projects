bf67144ecf5426c50322ba2471139bae1991c261
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:21:07 +0530

    feat(hdlbits): add more circuit solutions (p151-157) - timer FSM

---
+++ 01_HDLBits/14_more_circuits/p157_exams_2014q6b.v
@@ -0,0 +1,15 @@
+// HDLBits Problem 157: One-hot FSM next state
+// Author: Debjit Das | JGEC ECE
+
+module top_module (input d, done_counting, ack, input [9:0] state,
+    output B3_next, S_next, S1_next, Count_next, Wait_next,
+    output done, counting, shift_ena);
+    assign S_next    =(state[0]&~d)|(state[1]&~d)|(state[3]&~d)|(state[9]&ack);
+    assign S1_next   = state[0]&d;
+    assign B3_next   = state[6];
+    assign Count_next= state[7]|(state[8]&~done_counting);
+    assign Wait_next =(state[8]&done_counting)|(state[9]&~ack);
+    assign shift_ena = state[4]|state[5]|state[6]|state[7];
+    assign counting  = state[8];
+    assign done      = state[9];
+endmodule
