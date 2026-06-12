e2233edf23b518dde899e6d8e2ade82253c70fbb
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:16:13 +0530

    feat(hdlbits): add combinational circuit solutions (p44-60)

---
+++ 01_HDLBits/06_circuits_comb/p052_mt2013_q2.v
@@ -0,0 +1,6 @@
+// HDLBits Problem 52: Two-bit equality
+// Author: Debjit Das | JGEC ECE
+
+module top_module (input [1:0] A, input [1:0] B, output z);
+    assign z = (A == B) ? 1'b1 : 1'b0;
+endmodule
