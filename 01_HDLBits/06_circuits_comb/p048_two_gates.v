566e5c71f148aec88d8ea7965c656ff7ee9cdc9e
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:16:02 +0530

    feat(hdlbits): add combinational circuit solutions (p44-60)

---
+++ 01_HDLBits/06_circuits_comb/p048_two_gates.v
@@ -0,0 +1,6 @@
+// HDLBits Problem 48: Two gates
+// Author: Debjit Das | JGEC ECE
+
+module top_module (input in1, input in2, input in3, output out);
+    assign out = in3 ^ (~(in1 ^ in2));
+endmodule
