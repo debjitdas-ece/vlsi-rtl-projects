5fac928e6a7f46cadc8f6d47d4c086b08248f3ae
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:15:47 +0530

    feat(hdlbits): add more features solutions (p39-43) - generate, 100-bit

---
+++ 01_HDLBits/05_more_features/adder100i.v
@@ -0,0 +1,13 @@
+// HDLBits Problem 42: 100-bit ripple carry adder
+// Author: Debjit Das | JGEC ECE
+
+module top_module (input [99:0] a, b, input cin,
+    output [99:0] cout, output [99:0] sum);
+    genvar i;
+    generate
+        for (i=0; i<100; i=i+1) begin : fa
+            if (i==0) assign {cout[0], sum[0]} = a[0]+b[0]+cin;
+            else      assign {cout[i], sum[i]} = a[i]+b[i]+cout[i-1];
+        end
+    endgenerate
+endmodule
