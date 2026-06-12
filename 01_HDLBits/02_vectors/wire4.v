648361c306010c3cfa6b4f4f6d213398e77e9feb
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:14:03 +0530

    feat(hdlbits): add vector and basic gate solutions (p4-19)

---
+++ 01_HDLBits/02_vectors/wire4.v
@@ -0,0 +1,6 @@
+// HDLBits Problem 4: Connect 4 inputs to 4 outputs
+// Author: Debjit Das | JGEC ECE
+
+module top_module (input a, b, c, d, output w, x, y, z);
+    assign w = a; assign x = b; assign y = c; assign z = d;
+endmodule
