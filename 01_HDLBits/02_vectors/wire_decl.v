5171a32c470a15da920f6cf2bb992bbb5c2e37dd
Debjit Das <debjitdas.intern@gmail.com>
2026-06-13 02:14:16 +0530

    feat(hdlbits): add vector and basic gate solutions (p4-19)

---
+++ 01_HDLBits/02_vectors/wire_decl.v
@@ -0,0 +1,10 @@
+// HDLBits Problem 9: Wire declarations
+// Author: Debjit Das | JGEC ECE
+
+module top_module (input a, b, c, d, output out, out_n);
+    wire w1, w2;
+    assign w1    = a & b;
+    assign w2    = c & d;
+    assign out   = w1 | w2;
+    assign out_n = ~out;
+endmodule
